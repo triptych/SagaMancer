6e97c781ba9cedf22e6f84294a6f69d296d21f12
Andrew Wooldridge <triptych@whisperstorm.com>
2012-04-13 23:06:42 +0000

    adding yui js and some html

--- src/js/sagamancer.js
+++ src/js/sagamancer.js
@@ -1,1 +1,11 @@
 /** javascript logic for sagamancer **/
+YUI.add("sagamancer",function(Y){
+        Y.log("sagamancer added");
+    
+    },'0.0.1',{
+        requires: ['node']
+    });
+    
+YUI().use("node","sagamancer", function(Y){
+    Y.one("#loading").setContent("load happens");
+    });    
