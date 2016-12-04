4bfd78affa8b1c4b3f681d8b7a8dd4ffd9880740
Andrew Wooldridge <triptych@whisperstorm.com>
2016-12-04 16:39:34 +0000

    working through tutorial

--- test/src/game.js
+++ test/src/game.js
@@ -2,9 +2,13 @@
 window.onload = function(){
     var game = new Phaser.Game(500,500);
     var playGame = function(game){};
+    var tileSize = 80;
     playGame.prototype = {
         create: function(){
             console.log("That's my awesome game.");
+        },
+        preload: function(){
+            game.load.spritesheet("tiles", "tiles3.png", tileSize, tileSize);
         }
     }
     game.state.add("PlayGame", playGame);
