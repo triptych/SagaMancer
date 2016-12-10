d2571459ae4e2a1a923129565b9113d75bf0c1df
Andrew Wooldridge <triptych@whisperstorm.com>
2016-12-10 06:37:23 +0000

    getting code from other work I did earlier

--- test/src/game.js
+++ test/src/game.js
@@ -1,57 +1,84 @@
-/* global Phaser */
-window.onload = function(){
-    var game = new Phaser.Game(500,500);
-    var playGame = function(game){};
-    var tileSize = 80;
-    var numRows = 4;
-    var numCols = 5;
-    var tileSpacing = 10;
-    var tilesArray = [];
-    
-    playGame.prototype = {
-        create: function(){
-            console.log("in create");
-            //game.add.image(0,0,"tiles");
-            this.placeTiles();
-        },
-        preload: function(){
-            console.log("in preload")
-            game.load.spritesheet("tiles", "tiles3.png", tileSize, tileSize);
-        },
-        placeTiles: function(){
-            //function code goes here
-            console.log("placeTiles called");
-            var leftSpace = (game.width - (numCols * tileSize) - ((numCols - 1) * tileSpacing))/2;
-            var topSpace = (game.height - (numRows * tileSize) - ((numRows - 1) * tileSpacing))/2;
-            
-            for(var i = 0; i < numRows * numCols; i++){
-                tilesArray.push(Math.floor(i/2));
-            }
-            
-            console.log("my tile values: " + tilesArray);
-            
-            
-            for(i = 0; i < numCols; i++){
-                for(var j = 0; j < numRows; j++){
-                    //var tile = game.add.image(leftSpace+ i * (tileSize + tileSpacing), topSpace + j * (tileSize + tileSpacing), "tiles");
-                    var tile = game.add.button(leftSpace + i * (tileSize + tileSpacing), 
-                        topSpace + j * (tileSize + tileSpacing), 
-                        "tiles", 
-                        this.showTile,
-                        this);
-                        
-                    tile.frame = 10;
-                    tile.value = tilesArray[j * numCols + 1];
-                }    
-            }
-            
-        },
-        showTile: function(target){
-            console.log("showTile clicked. target value:", target.value);
-            target.frame = target.value;
+console.log("game.js loading");
+window.addEventListener("load", function(){
+  console.log("window load called");
+  var tileSize = 80; // px size of tiles
+  var numRows = 4;
+  var numCols = 5;
+  var tileSpacing = 10; // in px
+  var tilesArray = [];
+  var selectedArray = [];
+
+  var game = new Phaser.Game(500,500);
+  var playGame = function(game){}
+  playGame.prototype = {
+    preload: function(){
+      console.log("in preload");
+      game.load.spritesheet('tiles', 'tiles3.png', tileSize, tileSize);
+    },
+    create: function(){
+      console.log("In playGame.prototype");
+      //game.add.image(0,0,"tiles");
+      this.placeTiles();
+    },
+    placeTiles: function(){
+      console.log("in placeTiles");
+      var leftSpace = (game.width - (numCols * tileSize) - ((numCols - 1) * tileSpacing)) / 2;
+      var topSpace = (game.height - (numRows * tileSize)  - ((numRows - 1) * tileSpacing)) / 2;
+
+      for(var i = 0; i < numRows * numCols; i++) {
+       tilesArray.push(Math.floor(i / 2));
+      }
+      console.log("my tile values: " + tilesArray);
+
+      for(i = 0; i < numRows * numCols; i++){
+        var from = game.rnd.between(0,tilesArray.length-1);
+        var to = game.rnd.between(0, tilesArray.length-1);
+        var temp = tilesArray[from];
+        tilesArray[from] = tilesArray[to];
+        tilesArray[to] = temp;
+      }
+
+
+      for(var i=0; i < numCols; i++){
+        for (var j=0; j<numRows; j++){
+
+          var tile = game.add.button(
+            leftSpace + i * (tileSize + tileSpacing),
+            topSpace + j * (tileSize + tileSpacing), "tiles",
+            this.showTile, this);
+
+          tile.frame = 10;
+          tile.value = tilesArray[j * numCols + i];
         }
+      }
+    },
+    showTile: function(target){
+      console.log("show me!");
+      console.log("this tile has value = " + target.value);
+      if(selectedArray.length < 2 && selectedArray.indexOf(target) == -1){
+        target.frame = target.value;
+        selectedArray.push(target);
+      }
+
+      if(selectedArray.length == 2){
+        game.time.events.add(Phaser.Timer.SECOND, this.checkTiles, this);
+      }
+
+    },
+    checkTiles: function(){
+      if(selectedArray[0].value == selectedArray[1].value){
+        selectedArray[0].destroy();
+        selectedArray[1].destroy();
+      }
+      else{
+        selectedArray[0].frame = 10;
+        selectedArray[1].frame = 10;
+      }
+      selectedArray.length = 0;
+
     }
-    game.state.add("PlayGame", playGame);
-    game.state.start("PlayGame");
-    
-}
+  }
+
+  game.state.add("Playgame", playGame);
+  game.state.start("Playgame");
+});
